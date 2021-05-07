0a3219793175972e8395c7c2a3c344a98986595f
KrisQ <kquemener1@gmail.com>
2021-05-07 15:48:06 -0500

    Revert "Merge pull request #5 from getnacelle/DD-283"

--- src/components/ConfigScreen.tsx
+++ src/components/ConfigScreen.tsx
@@ -1,86 +1,54 @@
-import React, { Component } from 'react'
-import { AppExtensionSDK } from 'contentful-ui-extensions-sdk'
-import {
-  Button,
-  Card,
-  Form,
-  FormLabel,
-  Heading,
-  Note,
-  Paragraph,
-  Subheading,
-  TextInput,
-  TextLink,
-  HelpText,
-} from '@contentful/forma-36-react-components'
-import { css } from 'emotion'
-import logo from '../logo-dark.svg'
-
-export interface Space {
-  nacelleSpaceId: string
-  nacelleSpaceToken: string
-}
+import React, { Component } from 'react';
+import { AppExtensionSDK } from 'contentful-ui-extensions-sdk';
+import { Card, Form, FormLabel, Heading, Note, Paragraph, Subheading, TextInput, TextLink, HelpText } from '@contentful/forma-36-react-components';
+import { css } from 'emotion';
+import logo from '../logo-dark.svg';
 
 export interface AppInstallationParameters {
   nacelleSpaceId: string
   nacelleSpaceToken: string
-  spaces: Array<Space>
 }
 
 interface ConfigProps {
-  sdk: AppExtensionSDK
+  sdk: AppExtensionSDK;
 }
 
 interface ConfigState {
-  parameters: AppInstallationParameters
+  parameters: AppInstallationParameters;
 }
 
 export default class Config extends Component<ConfigProps, ConfigState> {
   state = {
     parameters: {
       nacelleSpaceId: '',
-      nacelleSpaceToken: '',
-      spaces: [
-        {
-          nacelleSpaceId: '',
-          nacelleSpaceToken: '',
-        },
-      ],
-    },
-  }
+      nacelleSpaceToken: ''
+    }
+  };
 
   constructor(props: ConfigProps, state: ConfigState) {
-    super(props)
+    super(props);
 
-    this.state = {
-      parameters: {
-        nacelleSpaceId: '',
-        nacelleSpaceToken: '',
-        spaces: [
-          {
-            nacelleSpaceId: '',
-            nacelleSpaceToken: '',
-          },
-        ],
-      },
-    }
+    this.state = { parameters: {
+      nacelleSpaceId: '',
+      nacelleSpaceToken: ''
+    } };
 
     // `onConfigure` allows to configure a callback to be
     // invoked when a user attempts to install the app or update
     // its configuration.
-    props.sdk.app.onConfigure(() => this.onConfigure())
+    props.sdk.app.onConfigure(() => this.onConfigure());
   }
 
   async componentDidMount() {
     // Get current parameters of the app.
     // If the app is not installed yet, `parameters` will be `null`.
-    const parameters: AppInstallationParameters | null = await this.props.sdk.app.getParameters()
+    const parameters: AppInstallationParameters | null = await this.props.sdk.app.getParameters();
 
     this.setState(parameters ? { parameters } : this.state, () => {
       // Once preparation has finished, call `setReady` to hide
       // the loading screen and present the app to a user.
-      this.props.sdk.app.setReady()
-    })
+      this.props.sdk.app.setReady();
+    });
   }
 
   onConfigure = async () => {
@@ -96,207 +64,82 @@ export default class Config extends Component<ConfigProps, ConfigState> {
       targetState: {
         EditorInterface: {
           // A content type id where we will assign the app to the sidebar
-          productGrid: {
-            // assignment to sidebar in position 0 (will show up at the very top of the sidebar)
-            sidebar: { position: 0 },
-          },
-        },
+          'productGrid': {
+              // assignment to sidebar in position 0 (will show up at the very top of the sidebar)
+              sidebar: { position: 0 }
+          }
+        }
       },
-    }
-  }
+    };
+  };
 
   onParameterChange = (key: string, e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.currentTarget
-    this.setState((state) => ({
-      parameters: { ...state.parameters, [key]: value },
-      // spaces: state.spaces,
-    }))
-  }
-
-  addSpace = () => {
-    this.setState((state) => {
-      const spaces = state.parameters.spaces
-        ? [
-            ...state.parameters.spaces,
-            {
-              nacelleSpaceId: '',
-              nacelleSpaceToken: '',
-            },
-          ]
-        : [
-            {
-              nacelleSpaceId: '',
-              nacelleSpaceToken: '',
-            },
-          ]
-      return {
-        parameters: {
-          ...state.parameters,
-          spaces,
-        },
-      }
-    })
-  }
-
-  removeSpace = (index: number) => {
-    this.setState((state) => {
-      let spaces = [...state.parameters.spaces]
-      spaces.splice(index, 1)
-      return {
-        parameters: {
-          ...state.parameters,
-          spaces,
-        },
-      }
-    })
-  }
-
-  onSpaceChange = (
-    key: string,
-    e: React.ChangeEvent<HTMLInputElement>,
-    index: number
-  ) => {
-    const { value } = e.currentTarget
-
-    this.setState((state) => {
-      let spaces = [...state.parameters.spaces]
-      spaces[index] = { ...spaces[index], [key]: value }
-      return {
-        parameters: { ...state.parameters, spaces },
-      }
-    })
-  }
+    this.setState(state => ({
+      parameters: { ...state.parameters, [key]: value }
+    }));
+  };
 
   render() {
     return (
-      <div className={css({ margin: '80px' })}>
+      <div
+        className={css({ margin: '80px' })}
+      >
         <Form>
           <Heading>Configuration</Heading>
-          <Note title='Nacelle x Contentful'>
+          <Note title="Nacelle x Contentful">
             <Paragraph>
-              Welcome, once you add your Nacelle Space info, we can properly
-              link accounts!
+              Welcome, once you add your Nacelle Space info, we can properly link accounts!
             </Paragraph>
           </Note>
 
           <Card>
             <Subheading>Nacelle Space Settings</Subheading>
             <HelpText>
-              (
-              <TextLink href='https://dashboard.getnacelle.com' target='_blank'>
-                Dashboard
-              </TextLink>
-              )
+              (<TextLink href="https://dashboard.getnacelle.com" target="_blank">Dashboard</TextLink>)
             </HelpText>
             <br />
-            <FormLabel htmlFor='nacelleSpaceId'>Nacelle Space Id</FormLabel>
+            <FormLabel htmlFor="nacelleSpaceId">
+              Nacelle Space Id
+            </FormLabel>
             <TextInput
-              name='nacelleSpaceId'
-              type='text'
-              width='large'
-              className='f36-margin-bottom--m'
-              placeholder='Nacelle Space Id'
-              value={this.state.parameters.nacelleSpaceId}
-              onChange={(event) =>
+              name="nacelleSpaceId"
+              type="text"
+              width="large"
+              className="f36-margin-bottom--m"
+              placeholder="Nacelle Space Id"
+              value={ this.state.parameters.nacelleSpaceId }
+              onChange={event =>
                 this.onParameterChange('nacelleSpaceId', event)
               }
             />
-            <FormLabel htmlFor='nacelleSpaceToken'>
+            <FormLabel htmlFor="nacelleSpaceToken">
               Nacelle Space Token
             </FormLabel>
             <TextInput
-              name='nacelleSpaceToken'
-              type='text'
-              width='large'
-              className='f36-margin-bottom--m'
-              placeholder='Nacelle Space Token'
-              value={this.state.parameters.nacelleSpaceToken}
-              onChange={(event) =>
+              name="nacelleSpaceToken"
+              type="text"
+              width="large"
+              className="f36-margin-bottom--m"
+              placeholder="Nacelle Space Token"
+              value={ this.state.parameters.nacelleSpaceToken }
+              onChange={event =>
                 this.onParameterChange('nacelleSpaceToken', event)
               }
             />
-            <Subheading>Other Spaces (optional)</Subheading>
-            <HelpText>
-              Useful if you want to be able to fetch data from multiple Nacelle
-              spaces.
-            </HelpText>
-            <br />
-            {this.state.parameters.spaces &&
-              this.state.parameters.spaces.map((space, index) => (
-                <div key={index}>
-                  <Subheading>Space #{index + 2}</Subheading>
-                  <br />
-                  <FormLabel htmlFor='nacelleSpaceId'>
-                    Nacelle Space Id
-                  </FormLabel>
-                  <TextInput
-                    name='nacelleSpaceId'
-                    type='text'
-                    width='large'
-                    className='f36-margin-bottom--m'
-                    placeholder='Nacelle Space Id'
-                    value={this.state.parameters.spaces[index].nacelleSpaceId}
-                    onChange={(event) =>
-                      this.onSpaceChange('nacelleSpaceId', event, index)
-                    }
-                  />
-                  <FormLabel htmlFor='nacelleSpaceToken'>
-                    Nacelle Space Token
-                  </FormLabel>
-
-                  <TextInput
-                    name='nacelleSpaceToken'
-                    type='text'
-                    width='large'
-                    className='f36-margin-bottom--m'
-                    placeholder='Nacelle Space Token'
-                    value={
-                      this.state.parameters.spaces[index].nacelleSpaceToken
-                    }
-                    onChange={(event) =>
-                      this.onSpaceChange('nacelleSpaceToken', event, index)
-                    }
-                  />
-                  <Button
-                    buttonType='muted'
-                    size='small'
-                    icon='Close'
-                    onClick={() => {
-                      this.removeSpace(index)
-                    }}
-                  >
-                    Remove Space
-                  </Button>
-                </div>
-              ))}
-            <br />
-            <Button
-              buttonType='muted'
-              icon='Plus'
-              onClick={() => {
-                this.addSpace()
-              }}
-            >
-              Add Space
-            </Button>
           </Card>
         </Form>
         <div
-          className={css({
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-          })}
+          className={css({ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' })}
         >
           <img
             className={css({ height: '30px', marginBottom: '10px' })}
-            src={logo}
-            alt='Nacelle Logo'
+            src={ logo }
+            alt="Nacelle Logo"
           />
-          <Paragraph>Copyright © {new Date().getFullYear()} Nacelle</Paragraph>
+            <Paragraph>Copyright © {new Date().getFullYear()} Nacelle</Paragraph>
         </div>
       </div>
-    )
+    );
   }
 }
--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -8,15 +8,12 @@ import {
   TextField,
   Tabs,
   Tab,
-  Dropdown,
-  DropdownList,
-  DropdownListItem,
 } from '@contentful/forma-36-react-components'
 import Paginator from './Paginator'
 import ResourceListItem from './ResourceListItem'
 import { css } from 'emotion'
 import { DialogExtensionSDK } from 'contentful-ui-extensions-sdk'
-import { AppInstallationParameters, Space } from './ConfigScreen'
+import { AppInstallationParameters } from './ConfigScreen'
 import NacelleClient, {
   NacelleGraphQLConnector,
   ProductOptions,
@@ -49,7 +46,6 @@ interface DialogState {
   valueKey: string
   searchValue: string
   publishedValue: string
-  isOpen: boolean
   resource: any
   resourceLabel: string
   resources: any[]
@@ -61,7 +57,6 @@ interface DialogState {
   selectedTabId: string
   loading: boolean
   storage: LocalForage
-  spaces: Space[]
   client: NacelleClient
   currentPage: {
     products: number
@@ -80,7 +75,6 @@ export default class Dialog extends Component<DialogProps, DialogState> {
       valueKey: 'handle',
       searchValue: '',
       publishedValue: '',
-      isOpen: false,
       resource: undefined,
       resourceLabel: '',
       resources: [],
@@ -92,7 +86,6 @@ export default class Dialog extends Component<DialogProps, DialogState> {
       selectedTabId: 'collections',
       loading: true,
       storage: localforage,
-      spaces: [],
       client: new NacelleClient({
         token: 'test',
         id: 'test',
@@ -106,8 +99,8 @@ export default class Dialog extends Component<DialogProps, DialogState> {
   }
 
   async componentDidMount() {
-    const { nacelleSpaceId: id, nacelleSpaceToken: token, spaces } = this.props
-      .sdk.parameters.installation as AppInstallationParameters
+    const { nacelleSpaceId: id, nacelleSpaceToken: token } = this.props.sdk
+      .parameters.installation as AppInstallationParameters
     const invocation = this.props.sdk.parameters.invocation as DialogState
     const { value } = invocation
 
@@ -122,9 +115,9 @@ export default class Dialog extends Component<DialogProps, DialogState> {
       ...invocation,
       publishedValue: value,
       loading: false,
-      spaces: [{ nacelleSpaceId: id, nacelleSpaceToken: token }, ...spaces],
     }))
   }
+
   setClient = async (id: string, token: string) => {
     const client = new NacelleClient({
       id,
@@ -138,23 +131,10 @@ export default class Dialog extends Component<DialogProps, DialogState> {
     this.setState(() => ({ client }))
   }
 
-  updateSpace = async (id: string, token: string) => {
-    this.setState({ loading: true })
-    this.setState(() => ({ isOpen: false }))
-    await this.setClient(id, token)
-    await this.setStorage(id)
-    await this.refreshData()
-    this.setState({ loading: false })
-  }
-
   setStorage = async (name: string) => {
     // Namespace storage with Space Id to avoid conflict if managing multiple spaces.
     const storage = localforage.createInstance({ name })
     this.setState(() => ({ storage }))
-  }
-
-  setOpen = (isOpen: boolean) => {
-    this.setState(() => ({ isOpen }))
   }
 
   refreshData = async (force: boolean = false) => {
@@ -174,9 +154,9 @@ export default class Dialog extends Component<DialogProps, DialogState> {
     let expires = (await this.state.storage.getItem(expirationKey)) as Date
 
     if (
-      resources &&
-      resources.length === 0 &&
-      (expires === null || date > expires)
+      (resources && resources.length === 0) ||
+      expires === null ||
+      date > expires
     ) {
       const connector = this.state.client.data
         .connector as NacelleGraphQLConnector
@@ -275,24 +255,26 @@ export default class Dialog extends Component<DialogProps, DialogState> {
     const startingIndex = pageIndex * itemsPerPage
 
     const searchedList = this.state.resources.filter((r) => {
-      const queryText = this.state.searchValue.toLowerCase().trim()
-      const titleMatch = r.title.toLowerCase().includes(queryText)
-      const handleMatch = r.handle.replace('/-/g', '').includes(queryText)
-      const tagsMatch =
-        Array.isArray(r.tags) &&
-        r.tags.find((tag: any) => tag.toLowerCase().includes(queryText))
-      const variantsMatch =
-        Array.isArray(r.variants) &&
-        r.variants.find((variant: any) => {
-          const titleMatch = variant.title.toLowerCase().includes(queryText)
-          const skuMatch =
-            variant.sku &&
-            variant.sku.toLowerCase().replace('/-/g', '').includes(queryText)
-          return titleMatch || skuMatch
-        })
-      return titleMatch || handleMatch || tagsMatch || variantsMatch
+      // const queryText = this.state.searchValue.toLowerCase().trim()
+      // const titleMatch = r.title.toLowerCase().includes(queryText)
+      // const handleMatch = r.handle.replace('/-/g', '').includes(queryText)
+      // const tagsMatch =
+      //   Array.isArray(r.tags) &&
+      //   r.tags.find((tag: any) => tag.toLowerCase().includes(queryText))
+      // const variantsMatch =
+      //   Array.isArray(r.variants) &&
+      //   r.variants.find((variant: any) => {
+      //     const titleMatch = variant.title.toLowerCase().includes(queryText)
+      //     const skuMatch =
+      //       variant.sku &&
+      //       variant.sku.toLowerCase().replace('/-/g', '').includes(queryText)
+      //     return titleMatch || skuMatch
+      //   })
+      // return titleMatch || handleMatch || tagsMatch || variantsMatch
 
-      // return r.title.toLowerCase().includes(this.state.searchValue.toLowerCase())
+      return r.title
+        .toLowerCase()
+        .includes(this.state.searchValue.toLowerCase())
     })
 
     const pageCount = Math.ceil(searchedList.length / itemsPerPage)
@@ -328,40 +310,6 @@ export default class Dialog extends Component<DialogProps, DialogState> {
           position: 'relative',
         })}
       >
-        <div>
-          {this.state.spaces && this.state.spaces.length > 1 && (
-            <Dropdown
-              isOpen={this.state.isOpen}
-              onClose={() => this.setOpen(false)}
-              toggleElement={
-                <Button
-                  size='small'
-                  buttonType='muted'
-                  indicateDropdown
-                  onClick={() => this.setOpen(!this.state.isOpen)}
-                >
-                  Trigger Dropdown
-                </Button>
-              }
-            >
-              <DropdownList>
-                {this.state.spaces.map((space) => (
-                  <DropdownListItem
-                    key={space.nacelleSpaceId}
-                    onClick={() =>
-                      this.updateSpace(
-                        space.nacelleSpaceId,
-                        space.nacelleSpaceToken
-                      )
-                    }
-                  >
-                    {space.nacelleSpaceId}
-                  </DropdownListItem>
-                ))}
-              </DropdownList>
-            </Dropdown>
-          )}
-        </div>
         <div className={css({ position: 'absolute', top: '0', right: '0' })}>
           <Button
             buttonType='muted'
