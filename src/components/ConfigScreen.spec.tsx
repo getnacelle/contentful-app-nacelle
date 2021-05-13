94e53c1f242e9feca33ac00767ac804fbe1e9841
KrisQ <kquemener1@gmail.com>
2021-05-13 10:21:49 -0500

    Test for space without spaces in config

--- src/components/ConfigScreen.spec.tsx
+++ src/components/ConfigScreen.spec.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import ConfigScreen from './ConfigScreen';
-import { render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import React from 'react'
+import ConfigScreen from './ConfigScreen'
+import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 describe('Config Screen component', () => {
   it('Component text exists', async () => {
@@ -9,16 +9,17 @@ describe('Config Screen component', () => {
       app: {
         onConfigure: jest.fn(),
         getParameters: jest.fn().mockReturnValueOnce({}),
-        setReady: jest.fn()
-      }
-    };
-    const { getByText } = render(<ConfigScreen sdk={mockSdk} />);
+        setReady: jest.fn(),
+      },
+    }
+
+    const { getByText } = render(<ConfigScreen sdk={mockSdk} />)
 
     // simulate the user clicking the install button
-    const configurationData = await mockSdk.app.onConfigure.mock.calls[0][0]();
+    const configurationData = await mockSdk.app.onConfigure.mock.calls[0][0]()
 
     expect(
       getByText('Welcome to your contentful app. This is your config page.')
-    ).toBeInTheDocument();
-  });
-});
+    ).toBeInTheDocument()
+  })
+})
--- src/components/Dialog.spec.tsx
+++ src/components/Dialog.spec.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import Dialog from './Dialog';
-import { render } from '@testing-library/react';
+import React from 'react'
+import Dialog from './Dialog'
+import { render } from '@testing-library/react'
+import { DialogExtensionSDK as sdk } from 'contentful-ui-extensions-sdk'
 
 describe('Dialog component', () => {
+  const mockSdk: any = {
+    window: {
+      startAutoResizer: jest.fn(),
+    },
+    parameters: {
+      installation: {
+        nacelleSpaceId: 'xxx',
+        nacelleSpaceToken: 'xxx',
+        // Test for apps with only one space
+        spaces: undefined,
+      },
+      invocation: {
+        value: 'xxx',
+      },
+    },
+  }
   it('Component text exists', () => {
-    const { getByText } = render(<Dialog />);
+    const { getByText } = render(<Dialog sdk={mockSdk} />)
 
-    expect(getByText('Hello Dialog Component')).toBeInTheDocument();
-  });
-});
+    expect(getByText('Products')).toBeInTheDocument()
+  })
+})
